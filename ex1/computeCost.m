ccf96691e768cb166174a529a4097bfd4cba9677
danimateos <daniel.mateos@amadeus.com>
2015-06-24 22:15:27 +0200

    Mandatory parts of ex1

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -7,7 +7,9 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+H = X * theta;
+
+J = (1/(2 * m)) * sum((H - y) .^ 2);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+     J_history(iter) = computeCost(X, y, theta);
 
-
-
-
-
+     theta -= (alpha / m) * (X' * (X * theta - y));
 
     % ============================================================
 
